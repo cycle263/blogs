c08d26bf4a1b651e703b70730c2db0172497d974
moltboy <cycle263@163.com>
2016-01-28 17:36:58 +0800

    Create rem.js

---
+++ mobile/rem.js
@@ -0,0 +1,16 @@
+/*************************************************/
+/* 解决移动端适应各种分辨率的rem方案之js代码核心 */
+/*************************************************/
+
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = ‘orientationchange’ in window ? ‘orientationchange’ : ‘resize’,
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 20 * (clientWidth / 320) + ‘px’;
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener(‘DOMContentLoaded’, recalc, false);
+})(document, window);
