a2536dd40a7d2f199064d17a5ed75893ec6554dc
moltboy <cycle263@163.com>
2016-02-02 17:29:54 +0800

    Update rem.js

--- mobile/rem.js
+++ mobile/rem.js
@@ -4,13 +4,13 @@
 
 (function (doc, win) {
     var docEl = doc.documentElement,
-        resizeEvt = ‘orientationchange’ in window ? ‘orientationchange’ : ‘resize’,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
         recalc = function () {
             var clientWidth = docEl.clientWidth;
             if (!clientWidth) return;
-            docEl.style.fontSize = 20 * (clientWidth / 320) + ‘px’;
+            docEl.style.fontSize = 20 * (clientWidth / 320) + 'px';
         };
     if (!doc.addEventListener) return;
     win.addEventListener(resizeEvt, recalc, false);
-    doc.addEventListener(‘DOMContentLoaded’, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
