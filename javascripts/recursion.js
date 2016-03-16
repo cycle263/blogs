77c677d25b89110be6080bca39446755134153f6
moltboy <cycle263@163.com>
2016-03-16 17:17:12 +0800

    Create recursion.js

---
+++ javascripts/recursion.js
@@ -0,0 +1,28 @@
+//数组生成树
+var data = [{"categoryName":"根节点","fatherId":0,"id":1},{"categoryName":"根节点","fatherId":1,"id":2},{"categoryName":"根节点","fatherId":0,"id":3}]
+var item, arr = [], obj = {};
+for(var i = 0, l = data.length; i < l; i++){
+   item = data[i];
+   item.children = [];
+   obj[item.id] = i;        //记住此节点在数组中的索引位置，以便于添加children
+   if(item.fatherId !== 0){
+      data[obj[item.fatherId]].children.push(item);     //添加children
+   }else{
+      arr.push(item);
+   }
+}
+console.log(arr);
+
+//递归树
+function searchTree(element, matchingTitle){
+     if(element.title == matchingTitle){
+          return element;
+     }else if (element.children != null){
+          var result = null;
+          for(i=0; result == null && i < element.children.length; i++){
+               result = searchTree(element.children[i], matchingTitle);
+          }
+          return result;
+     }
+     return null;
+}
