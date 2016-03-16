15dfd30097cb60aa534feb2737cf3c327d083929
moltboy <cycle263@163.com>
2016-03-16 17:18:27 +0800

    Update recursion.js
    递归树和生成树

--- javascripts/recursion.js
+++ javascripts/recursion.js
@@ -1,10 +1,11 @@
 //数组生成树
 var data = [{"categoryName":"根节点","fatherId":0,"id":1},{"categoryName":"根节点","fatherId":1,"id":2},{"categoryName":"根节点","fatherId":0,"id":3}]
 var item, arr = [], obj = {};
+
 for(var i = 0, l = data.length; i < l; i++){
    item = data[i];
    item.children = [];
-   obj[item.id] = i;        //记住此节点在数组中的索引位置，以便于添加children
+   obj[item.id] = i;             //记住此节点在数组中的索引位置，以便于添加children
    if(item.fatherId !== 0){
       data[obj[item.fatherId]].children.push(item);     //添加children
    }else{
@@ -13,7 +14,7 @@ for(var i = 0, l = data.length; i < l; i++){
 }
 console.log(arr);
 
-//递归树
+//递归树，生成list
 function searchTree(element, matchingTitle){
      if(element.title == matchingTitle){
           return element;
