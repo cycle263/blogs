71d0007cdb50aedc46f20ca670b3e32b9da574d8
moltboy <cycle263@163.com>
2016-03-31 16:11:26 +0800

    Update xhr.promise.js

--- javascripts/ECMAScript/example/xhr.promise.js
+++ javascripts/ECMAScript/example/xhr.promise.js
@@ -1,1 +1,27 @@
-new Promise(function(resolve, reject){})
+var promise = new Promise(function(resolve, reject){
+    var xhr = new XMLHttpRequeest();
+    xhr.open('GET', URL, true);
+    xhr.onload = function(){
+        if(xhr.status === 200){
+            resolve(xhr.responseText);
+        }else{
+            reject(new Error(req.statusText));
+        }
+    };
+    xhr.onerror = function(){
+        reject(new Error(req.statusText));
+    };
+    xhr.send();
+});
+
+promise.then(function(value){
+    console.log(value);
+}, function(error){
+    console.log(error);
+});
+
+promise.then(function(value){
+    console.log(value);
+}).catch(function(error){
+    console.log(error);
+});
