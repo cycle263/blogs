2ae3167b21b5e93d7ae7024ca70f76bd9851e7d2
moltboy <cycle263@163.com>
2016-05-27 16:52:04 +0800

    Update webpack.config.js

--- structure/webpack/example/webpack.config.js
+++ structure/webpack/example/webpack.config.js
@@ -1,9 +1,14 @@
 var path = require('path');
 
+//定义了一些文件夹的路径
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
 module.exports = {
     entry: './src/entry.js',
     output: {
-        path: path.join(__dirname, '/dist'),
+        path: path.resolve(__dirname, '/dist'),
         filename: 'bundle.js'
     },
     resolve: {
