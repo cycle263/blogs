a9f67e29ebb651521c9ccb2ab15d73c1872a9701
moltboy <cycle263@163.com>
2016-05-30 15:59:18 +0800

    Update webpack.config.js

--- structure/webpack/example/webpack.config.js
+++ structure/webpack/example/webpack.config.js
@@ -22,6 +22,9 @@ module.exports = {
             exclude: /node_modules/
         }]
     },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
     plugins: [
         new HtmlwebpackPlugin({
             title: 'PC app',
