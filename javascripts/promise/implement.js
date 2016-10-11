39b163f4a6a8c563e78bf0d6c114121daa50f44b
陈玉驰 <cycle263@163.com>
2016-10-11 17:05:09 +0800

    promise implement

--- javascripts/promise/implement.js
+++ javascripts/promise/implement.js
@@ -1,9 +1,51 @@
+/********** Promise的实现思路  *************/
+
 var promise = new Promise(function(resolve, reject){
 
 });
 
-function Promise(){
-  
+function Promise(executor){
+  var self = this;
+  self.status = 'pending';    // 初始化状态
+  self.data = undefined;
+  self.onResolvedCallback = [];
+  self.onRejectedCallback = [];
+
+  var resolve = function(){
+
+  };
+
+  var reject = function(){
+
+  };
+
+  try {     //  执行executor可能出现异常
+    executor(resolve, reject);
+  } catch(e) {
+    reject(e);
+  }
 }
+
+// =========>
+
+var resolve = function(value){
+  if(self.status === 'pending'){
+    self.status = 'resolved';
+    self.data = value;
+    for(var i = 0, l = self.onResolvedCallback.length; i++){
+      self.onResolvedCallback[i](value);
+    }
+  }
+};
+
+var reject = function(reason){
+  if(self.status === 'pending'){
+    self.status = 'rejected';
+    self.data = reason;
+    for(var i = 0, l = self.onRejectedCallback.length; i++){
+      self.onRejectedCallback[i](reason);
+    }
+  }
+};
 
 // =========>
