bd91e1a3c6eb83bca6c229bbe9bd00d3a2962902
陈玉驰 <cycle263@163.com>
2017-02-16 16:11:21 +0800

    loader

---
+++ structure/webpack/example/standard-config.js
@@ -0,0 +1,43 @@
+var webpack = require("webpack");
+var DefinePlugin = require('webpack/lib/DefinePlugin');
+module.exports =  {
+  context:process.cwd(),
+  watch: true,
+  entry: './index.js',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(process.cwd(),'dist/'),
+    filename: '[name].js'
+  },
+  resolve: {
+    alias:{ jquery: process.cwd()+'/src/lib/jquery.js', }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      _: 'underscore',
+      React: 'react'
+    }),
+    new DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development')
+      }
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.js[x]?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    },  {
+      test: /\.less$/,
+      loaders:['style-loader', 'css-loader','less-loader']
+    }, {
+      test: /\.(png|jpg|gif|woff|woff2|ttf|eot|svg|swf)$/,
+      loader: "file-loader?name=[name]_[sha512:hash:base64:7].[ext]"
+    }, {
+      test: /\.html/,
+      loader: "html-loader?" + JSON.stringify({minimize: false })
+    }]
+  }
+};
