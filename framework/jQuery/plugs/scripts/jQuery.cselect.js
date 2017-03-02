5fe1c5580190413215518d9c6c708d812c47a682
陈玉驰 <cycle263@163.com>
2017-03-02 15:33:45 +0800

    custom select

---
+++ framework/jQuery/plugs/scripts/jQuery.cselect.js
@@ -0,0 +1,217 @@
+(function($) {
+  $.fn.cselect = function(opts) {
+
+    // default option
+    var defaults = {
+      url: '',
+      method: 'POST'
+    };
+
+    $.extend(opts, defaults);
+
+    this.each(function(key, ele){
+      var select = $(ele),
+        parent = select.parent(),
+        items = parent.find('.select-items'),
+        currentRequest = null,
+        inputStr = '<li class="input"><input class="hidden-input" type="text" /></li>',
+        innerStr = '<span class="close">x</span><span class="before"></span><span class="after"></span>',
+        requestData = function(name){
+          currentRequest = jQuery.ajax({
+            url: opts.url,
+            type: opts.method,
+            data: jQuery.extend(opts.data, {name: encodeURIComponent(name)}),
+            beforeSend: function()    {
+              if(currentRequest != null) {
+                currentRequest.abort();
+              }
+            },
+            success: function(res) {
+              if(res) {
+                var str = '',
+                  selectedItem = select.find('li').map(function(k, ele){
+                    return $(ele).data('id');
+                  }).toArray();
+                for(var i = 0, l = res.length; i < l; i++){
+                  var item = res[i], selected = '';
+                  for(var j = 0, len = selectedItem.length; j < len; j++){
+                    if(item.id === selectedItem[j]){
+                      selected = 'selected';
+                    }
+                  }
+                  str += '<li class="' + selected + '" data-id="' + item.id + '">' + (item.name || item.text || "") + '</li>';
+                }
+                items.html(str).css({'top': select.clientHeight + 1, 'width': select.width()}).show();
+                currentRequest = null;
+              }
+            } else {
+              items.html('搜索为空').css({'top': select.clientHeight + 1, 'width': select.width()}).show();
+            }
+          });
+        };
+
+      var initHtml = function(){
+
+      };
+
+      var initEvent = function(){
+        select.on('keyup', '.c-select .input input', function(){
+          var me = $(this),
+            t = me.val();
+          me.css('width', (t.length * 13 + 12) + 'px');
+          requestData(t);
+        });
+        select.on('click', '.c-select .input input', function(event){
+          event.stopPropagation();
+        });
+
+        $('body').click(function(){
+          select.find('input').remove();
+          items.hide();
+        });
+        parent.on('click', '.c-select .before,.c-select .after,.c-select', function(event){
+          event.stopPropagation();
+          select.find('input').remove();
+          if($(this).hasClass('c-select')){
+            var mx = event.offsetX,
+              my = event.offsetY,
+              targetEle = null;
+            $(this).find('li').each(function(k,ele){
+              var ex = ele.offsetLeft,
+                ey = ele.offsetTop,
+                ew = ele.offsetWidth,
+                eh = ele.offsetHeight;
+              if(my >= ey && my <= ey + eh){
+                targetEle = ele;
+              }
+            });
+            if(targetEle !== null){
+              $(inputStr).insertAfter(targetEle).find('input').focus();
+            }else{
+              $(inputStr).appendTo(this).find('input').focus();
+            }
+          } else if($(this).hasClass('before')){
+            $(inputStr).insertBefore($(this).parent()).find('input').focus();
+          } else if($(this).hasClass('after')){
+            $(inputStr).insertAfter($(this).parent()).find('input').focus();
+          }
+        });
+        select.on('click', 'li:not(.input)', function(event){
+          event.stopPropagation();
+          select.find('input').remove();
+          $(inputStr).insertAfter(this).find('input').focus();
+        });
+
+        select.on('click', '.close', function(event){
+          event.stopPropagation();
+          $(this).parent().remove();
+        });
+
+        items.on('click', 'li', function(){
+          if($(this).hasClass('selected')) return;
+          select.find('input').replaceWith($(this.outerHTML).append(innerStr));
+          items.hide();
+        });
+      };
+
+      initHtml();
+      initEvent();
+    });
+    return this;
+  };
+})(jQuery);
+
+/*
+.c-select {
+  width: 280px;
+  max-height: 300px;
+  min-height: 36px;
+  padding: 4px 2px;
+  margin: 0;
+  border: 1px solid #aaa;
+  border-radius: 5px;
+  overflow: auto;
+}
+.c-select li{
+  display: inline-block;
+  position: relative;
+  list-style: none;
+  padding: 2px 0px 2px 16px;
+  border: 1px solid #aaa;
+  border-radius: 5px;
+  margin: 3px 8px;
+  background-color: #e4e4e4;
+  color: #555;
+  font-size: 14px;
+  white-space: nowrap;
+}
+.c-select li .before, .c-select li .after{
+  display: inline-block;
+  position: absolute;
+  width: 8px;
+  height: 24px;
+  top: 0;
+}
+.c-select li .before{
+  left: -9px;
+}
+.c-select li .after{
+  right: -9px;
+}
+.c-select li .close{
+  display: inline-block;
+  height: 20px;
+  width: 24px;
+  line-height: 20px;
+  text-align: center;
+  color: #999;
+  cursor: pointer;
+}
+.c-select li.input{
+  padding: 0;
+  outline: none;
+  border: none;
+  margin: 0;
+}
+.c-select li.input input{
+  outline: none;
+  font-size: 13px;
+}
+.hidden-input {
+  border: none;
+  width: 24px;
+  padding: 6px;
+  height: 32px;
+  display: inline-block;
+}
+.select-items {
+  width: 280px;
+  max-height: 300px;
+  position: absolute;
+  top: 75px;
+  z-index: 9;
+  background: #fff;
+  color: #666;
+  border: 1px solid #999;
+  box-shadow: 2px 3px 3px rgba(0, 0, 0, 0.2);
+  border-radius: 3px;
+  overflow: hidden;
+  padding: 2px;
+  margin: 0;
+  overflow: auto;
+}
+.select-items li {
+  cursor: pointer;
+  padding: 4px 16px;
+  list-style: none;
+  line-height: 24px;
+  white-space: nowrap;
+}
+.select-items li:hover{
+  background-color: #5897fb;
+  color: #fff;
+}
+.select-items li.selected {
+  background-color: #ddd;
+}
+*/
