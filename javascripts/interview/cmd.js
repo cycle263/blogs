cfccc29d88bcd35c255f71e7057de86a6c08656b
陈玉驰 <cycle263@163.com>
2017-04-20 10:03:39 +0800

    git command

--- javascripts/interview/cmd.js
+++ javascripts/interview/cmd.js
@@ -4,3 +4,4 @@ $$('.js-header-wrapper,.pagehead,.js-zeroclipboard-container,.commit-tease,.file
 });
 $$('.entry-content')[0].style.padding = '8px';
 $$('.new-discussion-timeline')[0].style.width = '100%';
+$$('.file')[0].style.margin = '0';
