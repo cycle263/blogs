fe69019fabbea8374466cd1399826388ef3d3231
陈玉驰 <cycle263@163.com>
2017-09-06 19:55:37 +0800

    calendar

--- framework/jQuery/plugs/scripts/jQuery.calendar.js
+++ framework/jQuery/plugs/scripts/jQuery.calendar.js
@@ -16,6 +16,13 @@
     var days_per_month = new Array(31, 28 + isLeap(y), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
     var tr_num = Math.ceil((dayOfWeek + days_per_month[m]) / 7);  // 总行数
     var str = '<table class="date-table"><thead><tr><th>星期日</th><th>星期一</th><th>星期二</th><th>星期三</th><th>星期四</th><th>星期五</th><th>星期六</th></tr></thead><tbody>';
+    var renderContent = function(arr){
+      var cstr = '';
+      for(var m = 0; m < arr.length; m++){
+        cstr += '<div class="content"><span class="info">'+arr[m].time+'</span><a class="info" href="' + arr[m].link + '" target="_blank">' + arr[m].name + '</a></div>';
+      }
+      return cstr;
+    };
 
     for(var i = 0; i < tr_num; i++){
       str += '<tr>';
@@ -23,14 +30,14 @@
         var index = i * 7 + j;  // 单元格index
         var date_temp = index - dayOfWeek + 1;
         var date = date_temp <= 0 ? days_per_month[m] + date_temp : date_temp > days_per_month[m] ? date_temp - days_per_month[m] : date_temp;
-        var temp = null;
+        var temp = [];
         var isThisMonth = !(date_temp <= 0 || date_temp > days_per_month[m]);   // 是否是本月日期
         for(var k = 0; k < data.length; k++){   // 展示对应日期的数据
           if(data[k].day === date){
-            temp = data[k];
+            temp.push(data[k]);
           }
         }
-        str += '<td ' + (date === day && month === m && year === y ? 'class="today"' : '') + '><div class="date-container"><span class="date">' + (!isThisMonth ? (index > 20 ? (m + 2 > 12 ? 1 : m + 2) : m) + '月' + date + '日' : date) + '</span>' + (temp && temp.name && isThisMonth ? '<a href="' + temp.link + '" target="_blank">' + temp.name + '</a>' : '') + '</div></td>';
+        str += '<td ' + (date === day && month === m && year === y ? 'class="today"' : '') + '><div class="date-container"><span class="date">' + (!isThisMonth ? (index > 20 ? (m + 2 > 12 ? 1 : m + 2) : m) + '月' + date + '日' : date) + '</span>' + (temp && temp.length && isThisMonth ? renderContent(temp) : '') + '</div></td>';
       }
       str += '</tr>';
     }
