6aff8ad0174f7edae684c213de006080627a334a
陈玉驰 <cycle263@163.com>
2018-03-27 17:55:25 +0800

    整理context

--- team/interview/cmd.js
+++ team/interview/cmd.js
@@ -1,15 +1,16 @@
 // interview 源码文件打印样式调试
-$$('.js-header-wrapper,.pagehead,.js-zeroclipboard-container,.commit-tease,.file-header,.site-footer-container,.footer').forEach(function(ele){
+$$('.js-header-wrapper,.file-navigation,.pagehead,.js-zeroclipboard-container,.commit-tease,.file-header,.site-footer-container,.footer').forEach(function(ele){
   ele.parentNode.removeChild(ele);
 });
 $$('.entry-content')[0].style.padding = '0px';
 $$('.new-discussion-timeline')[0].style.width = '100%';
 $$('.file')[0].style.margin = '0';
-$$('.markdown-body h2').forEach(function(ele){
-  ele.style.padding = '0px';
-  ele.style.marginTop = '8px';
+$$('.markdown-body li>p, .markdown-body li, .markdown-body h2').forEach(function (ele) {
+  ele.style.marginBottom = '6px';
+  ele.style.marginTop = '6px';
+  ele.style.fontSize = '14px';
+  ele.style.lineHeight = '22px';
 });
-
 
 
 
